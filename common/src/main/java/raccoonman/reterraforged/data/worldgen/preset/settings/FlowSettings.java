61cb192e7bee242b9a734a5cec0ee3449e3f064a
Edward <lucky_eddy@hotmail.com>
2026-08-07 06:50:01 +0800

    reorganise

--- common/src/main/java/raccoonman/reterraforged/data/worldgen/preset/settings/FlowSettings.java
+++ common/src/main/java/raccoonman/reterraforged/data/worldgen/preset/settings/FlowSettings.java
@@ -4,26 +4,42 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 
 public class FlowSettings {
+
+    // member settings
+    public boolean flowParticles;
+    public boolean boatFlowDynamics;
+    public boolean navigableWaterfalls;
+
+    // serialization handlers for codec
+    private boolean serializeFlowParticles() { return this.flowParticles; }
+    private boolean serializeBoatFlowDynamics() { return this.boatFlowDynamics; }
+    private boolean serializeNavigableWaterfalls() { return this.navigableWaterfalls; }
+
+    // Codec for writing to disk
     public static final Codec<FlowSettings> CODEC = RecordCodecBuilder.create(instance -> instance.group(
             Codec.BOOL.optionalFieldOf("flowParticles", true).forGetter(FlowSettings::serializeFlowParticles),
             Codec.BOOL.optionalFieldOf("boatFlowDynamics", true).forGetter(FlowSettings::serializeBoatFlowDynamics),
             Codec.BOOL.optionalFieldOf("navigableWaterfalls", true).forGetter(FlowSettings::serializeNavigableWaterfalls)
     ).apply(instance, FlowSettings::new));
 
-    public boolean flowParticles;
-    public boolean boatFlowDynamics;
-    public boolean navigableWaterfalls;
-
+    // default constructor
     public FlowSettings() {
         this(true, true, true);
     }
 
+    // value constructor
     public FlowSettings(boolean flowParticles, boolean boatFlowDynamics, boolean navigableWaterfalls) {
         this.flowParticles = flowParticles;
         this.boatFlowDynamics = boatFlowDynamics;
         this.navigableWaterfalls = navigableWaterfalls;
     }
 
+    // explicit default constructor used by presets
+    public static FlowSettings makeDefault() {
+        return new FlowSettings(true, true, true);
+    }
+
+    // Allow settings duplication
     public FlowSettings copy() {
         return new FlowSettings(
             this.flowParticles,
@@ -32,6 +48,7 @@ public class FlowSettings {
         );
     }
 
+    // Static cache of settings of current preset, used by mixins
     public class CurrentPresetState {
         private static FlowSettings currentSettings = new FlowSettings();
 
@@ -43,14 +60,6 @@ public class FlowSettings {
             currentSettings = settings != null ? settings : new FlowSettings();
         }
     }
-    public static FlowSettings makeDefault() {
-        return new FlowSettings(true, true, true);
-    }
-
-    private boolean serializeFlowParticles() { return this.flowParticles; }
-    private boolean serializeBoatFlowDynamics() { return this.boatFlowDynamics; }
-    private boolean serializeNavigableWaterfalls() { return this.navigableWaterfalls; }
-
     public static boolean enableFlowParticles() { return CurrentPresetState.currentSettings.flowParticles; }
     public static boolean enableBoatFlowDynamics() { return CurrentPresetState.currentSettings.boatFlowDynamics; }
     public static boolean enableNavigableWaterfalls() { return CurrentPresetState.currentSettings.navigableWaterfalls; }
