87ccb84ffefaee1cea62ffdd91003de07ee52510
Edward <lucky_eddy@hotmail.com>
2026-08-15 21:10:37 +0800

    Revert "Revert "Add flow field dynamics to rivers""

---
+++ fabric/src/main/java/raccoonman/reterraforged/fabric/RTFFabricClient.java
@@ -0,0 +1,22 @@
+package raccoonman.reterraforged.fabric;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
+import net.minecraft.client.Minecraft;
+import raccoonman.reterraforged.client.debug.FlowFieldDebugRenderer;
+
+public class RTFFabricClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+        Minecraft mc = Minecraft.getInstance();
+        WorldRenderEvents.AFTER_TRANSLUCENT.register(context -> {
+            FlowFieldDebugRenderer.render(
+                    context.matrixStack(),
+                    context.camera(),
+                    mc.renderBuffers().bufferSource()
+            );
+        });
+    }
+}
