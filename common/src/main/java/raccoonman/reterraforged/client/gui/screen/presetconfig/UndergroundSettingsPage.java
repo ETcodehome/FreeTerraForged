f82878220ee30d7fd0a4042c36e227bcb84877b6
Edward <lucky_eddy@hotmail.com>
2026-09-04 23:11:13 +0800

    Remove dud cave carver settings, shift underground controls to underground page.

--- common/src/main/java/raccoonman/reterraforged/client/gui/screen/presetconfig/UndergroundSettingsPage.java
+++ common/src/main/java/raccoonman/reterraforged/client/gui/screen/presetconfig/UndergroundSettingsPage.java
@@ -9,9 +9,10 @@ import raccoonman.reterraforged.client.gui.screen.page.LinkedPageScreen.Page;
 import raccoonman.reterraforged.client.gui.screen.presetconfig.PresetListPage.PresetEntry;
 import raccoonman.reterraforged.client.gui.widget.Slider;
 import raccoonman.reterraforged.data.worldgen.preset.settings.CaveSettings;
+import raccoonman.reterraforged.data.worldgen.preset.settings.ClimateSettings;
 import raccoonman.reterraforged.data.worldgen.preset.settings.Preset;
 
-public class CaveSettingsPage extends PresetEditorPage {
+public class UndergroundSettingsPage extends PresetEditorPage {
 	private Slider entranceCaveProbability;
 	private Slider cheeseCaveDepthOffset;
 	private Slider cheeseCaveProbability;
@@ -19,14 +20,20 @@ public class CaveSettingsPage extends PresetEditorPage {
 	private Slider noodleCaveProbability;
 	private Slider ravineProbability;
 	private CycleButton<Boolean> largeOreVeins;
+
+	private Slider undergroundBiomeSize;
+	private Slider undergroundBiomeVerticalSize;
+	private Slider undergroundBiomeCoverage;
+	private Slider undergroundBiomeClimateInfluence;
+	private CycleButton<Boolean> undergroundBiomeBanding;
 	
-	public CaveSettingsPage(PresetConfigScreen screen, PresetEntry preset) {
+	public UndergroundSettingsPage(PresetConfigScreen screen, PresetEntry preset) {
 		super(screen, preset);
 	}
 
 	@Override
 	public Component title() {
-		return Component.translatable(RTFTranslationKeys.GUI_CAVE_SETTINGS_TITLE);
+		return Component.translatable(RTFTranslationKeys.GUI_UNDERGROUND_SETTINGS_TITLE);
 	}
 
 	@Override
@@ -35,6 +42,18 @@ public class CaveSettingsPage extends PresetEditorPage {
 
 		Preset preset = this.preset.getPreset();
 		CaveSettings caves = preset.caves();
+		ClimateSettings climate = preset.climate();
+		ClimateSettings.BiomeShape biomeShape = climate.biomeShape;
+
+		int maximumUndergroundVerticalSize = PresetSettingsBounds.maximumUndergroundBiomeVerticalSize(
+				preset.world().properties.worldHeight,
+				preset.world().properties.worldDepth
+		);
+
+		biomeShape.undergroundBiomeVerticalSize = Math.min(
+				biomeShape.undergroundBiomeVerticalSize,
+				maximumUndergroundVerticalSize
+		);
 
 		this.entranceCaveProbability = PresetWidgets.createFloatSlider(caves.entranceCaveProbability, 0.0F, 1.0F, RTFTranslationKeys.GUI_SLIDER_ENTRANCE_CAVE_PROBABILITY, (slider, value) -> {
 			caves.entranceCaveProbability = (float) slider.scaleValue(value);
@@ -64,6 +83,26 @@ public class CaveSettingsPage extends PresetEditorPage {
 			caves.largeOreVeins = value;
 		});
 
+		this.undergroundBiomeSize = PresetWidgets.createIntSlider(biomeShape.undergroundBiomeSize, ClimateSettings.BiomeShape.MIN_BIOME_SIZE, ClimateSettings.BiomeShape.MAX_BIOME_SIZE, RTFTranslationKeys.GUI_SLIDER_UNDERGROUND_BIOME_SIZE, (slider, value) -> {
+			biomeShape.undergroundBiomeSize = (int) slider.scaleValue(value);
+			return value;
+		});
+		this.undergroundBiomeVerticalSize = PresetWidgets.createIntSlider(biomeShape.undergroundBiomeVerticalSize, ClimateSettings.BiomeShape.MIN_UNDERGROUND_VERTICAL_SIZE, maximumUndergroundVerticalSize, RTFTranslationKeys.GUI_SLIDER_UNDERGROUND_BIOME_VERTICAL_SIZE, (slider, value) -> {
+			biomeShape.undergroundBiomeVerticalSize = (int) slider.scaleValue(value);
+			return value;
+		});
+		this.undergroundBiomeCoverage = PresetWidgets.createFloatSlider(biomeShape.undergroundBiomeCoverage, 0.0F, 1.0F, RTFTranslationKeys.GUI_SLIDER_UNDERGROUND_BIOME_COVERAGE, (slider, value) -> {
+			biomeShape.undergroundBiomeCoverage = (float) slider.scaleValue(value);
+			return value;
+		});
+		this.undergroundBiomeClimateInfluence = PresetWidgets.createFloatSlider(biomeShape.undergroundBiomeClimateInfluence, 0.0F, 1.0F, RTFTranslationKeys.GUI_SLIDER_UNDERGROUND_BIOME_CLIMATE_INFLUENCE, (slider, value) -> {
+			biomeShape.undergroundBiomeClimateInfluence = (float) slider.scaleValue(value);
+			return value;
+		});
+		this.undergroundBiomeBanding = PresetWidgets.createToggle(biomeShape.undergroundBiomeBanding, RTFTranslationKeys.GUI_BUTTON_UNDERGROUND_BIOME_BANDING, (button, value) -> {
+			biomeShape.undergroundBiomeBanding = value;
+		});
+
 		this.left.addWidget(PresetWidgets.createLabel(RTFTranslationKeys.GUI_LABEL_NOISE_CAVES));
 		this.left.addWidget(this.entranceCaveProbability);
 		this.left.addWidget(this.cheeseCaveDepthOffset);
@@ -74,6 +113,13 @@ public class CaveSettingsPage extends PresetEditorPage {
 		this.left.addWidget(PresetWidgets.createLabel(RTFTranslationKeys.GUI_LABEL_CARVERS));
 		this.left.addWidget(this.ravineProbability);
 		this.left.addWidget(this.largeOreVeins);
+
+		this.left.addWidget(PresetWidgets.createLabel(RTFTranslationKeys.GUI_LABEL_UNDERGROUND_BIOMES));
+		this.left.addWidget(this.undergroundBiomeSize);
+		this.left.addWidget(this.undergroundBiomeVerticalSize);
+		this.left.addWidget(this.undergroundBiomeCoverage);
+		this.left.addWidget(this.undergroundBiomeClimateInfluence);
+		this.left.addWidget(this.undergroundBiomeBanding);
 	}
 	
 	@Override
