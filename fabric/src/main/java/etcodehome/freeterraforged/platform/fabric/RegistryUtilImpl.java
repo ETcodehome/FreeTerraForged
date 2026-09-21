579bd498abbce908a26f145c0cfc3f7427e4934c
Edward <lucky_eddy@hotmail.com>
2026-09-21 20:47:18 +0800

    cleanup RegistryUtil

--- fabric/src/main/java/etcodehome/freeterraforged/platform/fabric/RegistryUtilImpl.java
+++ fabric/src/main/java/etcodehome/freeterraforged/platform/fabric/RegistryUtilImpl.java
@@ -1,32 +1,17 @@
 package etcodehome.freeterraforged.platform.fabric;
 
 import com.mojang.serialization.Codec;
-
 import net.fabricmc.fabric.api.event.registry.DynamicRegistries;
 import net.fabricmc.fabric.api.event.registry.FabricRegistryBuilder;
-import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
 import net.minecraft.core.Registry;
-import net.minecraft.resources.RegistryDataLoader;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.world.level.GameRules;
 import etcodehome.freeterraforged.FTFCommon;
 
-import java.util.List;
-
+/**
+ * RegistryUtil Implementation specific to Fabric.
+ * Compile time resolved by Architectury Transformer.
+ */
 public class RegistryUtilImpl {
-	
-//	public static <T> WritableRegistry<T> getWritable(Registry<T> registry) {
-//		return (WritableRegistry<T>) registry;
-//	}
-//
-//	@SuppressWarnings("unchecked")
-//	public static <T> Registry<T> createRegistry(ResourceKey<? extends Registry<T>> key) {
-//		return FabricRegistryBuilder.createSimple((ResourceKey<Registry<T>>) key).buildAndRegister();
-//	}
-//
-//	public static <T> void createDataRegistry(ResourceKey<? extends Registry<T>> key, Codec<T> codec) {
-//		DynamicRegistries.register(key, codec);
-//	}
 
 	public static <T> void register(Registry<T> registry, String name, T value) {
 		Registry.register(registry, FTFCommon.location(name), value);
@@ -38,17 +23,9 @@ public class RegistryUtilImpl {
 
 	public static <T> void createDataRegistry(ResourceKey<Registry<T>> key, Codec<T> codec, boolean synced) {
 		if(synced) {
-			DynamicRegistries.registerSynced(key, codec); // TODO what does SyncOption.SKIP_WHEN_EMPTY do?
+			DynamicRegistries.registerSynced(key, codec);
 		} else {
 			DynamicRegistries.register(key, codec);
 		}
-	}
-
-	public static <T extends GameRules.Value<T>> GameRules.Key<T> registerGameRule(String name, GameRules.Category category, GameRules.Type<T> type) {
-		return GameRuleRegistry.register(name, category, type);
-	}
-
-	public static List<RegistryDataLoader.RegistryData<?>> getDynamicRegistries() {
-		return DynamicRegistries.getDynamicRegistries();
 	}
 }
